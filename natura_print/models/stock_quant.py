3cde3aa7daf6d6bdac49b21e007ed039094a82a5
Aaron T <aaron.t@natura.io>
2026-01-19 14:10:36 -0800

    upgrade fix

--- natura_print/models/stock_quant.py
+++ natura_print/models/stock_quant.py
@@ -4,6 +4,10 @@ from odoo import models
 class StockQuant(models.Model):
     _inherit = "stock.quant"
 
+    # Compatibility shim: some environments include views referencing stock.quant.reason_note.
+    # Keeping this field here prevents upgrade failures until those views are cleaned up.
+    reason_note = fields.Text(string="Reason Note")
+
     def action_open_print_wizard(self):
         action = self.env.ref("natura_print.action_natura_print_quant_label_wizard").read()[0]
         ids = self.env.context.get("active_ids") or self.ids
