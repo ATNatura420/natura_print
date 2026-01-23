a892fe2a4ee3f31a58402a7a25675663d2f2edcc
Aaron T <aaron.t@natura.io>
2026-01-22 18:27:57 -0800

    model selection fallback on label automation rule 04

--- natura_print/wizards/label_automation_wizard.py
+++ natura_print/wizards/label_automation_wizard.py
@@ -22,8 +22,19 @@ class NaturaPrintLabelAutomationWizard(models.TransientModel):
         if not self.automation_id or not self.automation_id.webhook_url:
             raise UserError(_("Missing label automation rule or webhook URL."))
 
+        if self.automation_id.model_id and self.automation_id.model_id.model != self.source_model:
+            raise UserError(
+                _(
+                    "Selected automation rule is for %(rule_model)s, but this wizard is for %(source_model)s."
+                )
+                % {
+                    "rule_model": self.automation_id.model_id.model,
+                    "source_model": self.source_model,
+                }
+            )
+
         payload = {
-            "_model": self.source_model,
+            "_model": self.automation_id.model_id.model or self.source_model,
             "_id": self.source_res_id,
         }
         if self.qty:
